68dcc1ed1cff4ecaf1ec49d0b72cb88ab85f4e81
tanglimei <562630424@qq.com>
2015-12-10 09:27:07 +0800

    第一次创建项目提交
    9：26 新创建的项目提交

---
+++ 1210/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1210
+//
+//  Created by 悠然天地 on 15/12/10.
+//  Copyright © 2015年 KuaiYong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 1210/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1210
+//
+//  Created by 悠然天地 on 15/12/10.
+//  Copyright © 2015年 KuaiYong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
