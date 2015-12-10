7f7e6de5d498918e2ad02d25e0312a627db32e3e
tanglimei <562630424@qq.com>
2015-12-10 09:41:00 +0800

    增加了好多东西，比如Public

---
+++ Public/Logout.h
@@ -0,0 +1,15 @@
+//
+//  Logout.h
+//  Framework1
+//
+//  Created by 悠然天地 on 15/11/11.
+//  Copyright © 2015年 My. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Logout : NSObject
+
+-(void)log33:(NSString *)message;
+
+@end
---
+++ Public/Logout.m
@@ -0,0 +1,15 @@
+//
+//  Logout.m
+//  Framework1
+//
+//  Created by 悠然天地 on 15/11/11.
+//  Copyright © 2015年 My. All rights reserved.
+//
+
+#import "Logout.h"
+
+@implementation Logout
+-(void)log33:(NSString *)message{
+    NSLog(@"cdfdf%@",message);
+}
+@end
